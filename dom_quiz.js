3cdbddfa31f418e730c89ab42c4a0e9a932c2c11
jgerardsimcock <justin_simcock@yahoo.com>
2014-06-01 11:55:47 -1000

    Submit event added

--- dom_quiz.js
+++ dom_quiz.js
@@ -1,7 +1,28 @@
 (function(){
 
-var answer1 = document.getElementById("question");
-answer1.innerHTML = answer1.innerHTML.replace("","Where are the biggest waves in Oahu?");
+var score = 0
 
+var question = ["Where are biggest waves in Oahu?", "What is the freaky wave called near Portlock?", "What is the name of the beach park where Pipeline is located?", "What is a surfboard made of?", "What is the middle wodden spine of a surfboard called?", "What are young surfers called?"];
+var response = ["Waimea", "China Walls", "Ehukai", "Foam", "Stringer", "Wax", "Groms"];
+
+for (var i = 0; i < question.length; i++) {
+  var response = document.getElementById("question");
+  response.innerHTML = response.innerHTML.replace("", question[i]);
+
+function checkAnswer(){
+  document.getElementById('answer').submit();
+    if (question.answer = response) {
+      alert("Right on!");
+      ++score;
+      }
+    else {
+      alert("Sorry, wrong answer!");
+    }
+  }
+};
+
+
+
+// submit.onclick
 
 })();
