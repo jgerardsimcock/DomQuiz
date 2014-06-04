5fbadcbde7a54e07eca86835ee00abf8f994355c
jgerardsimcock <justin_simcock@yahoo.com>
2014-06-03 19:55:54 -1000

    Nested For Loops

--- dom_quiz.js
+++ dom_quiz.js
@@ -1,26 +1,54 @@
 (function(){
 
-// var score = 0
-
-// var question = ["Where are biggest waves in Oahu?", "What is the freaky wave called near Portlock?", "What is the name of the beach park where Pipeline is located?", "What is a surfboard made of?", "What is the middle wodden spine of a surfboard called?", "What are young surfers called?"];
-// var response = ["Waimea", "China Walls", "Ehukai", "Foam", "Stringer", "Wax", "Groms"];
-
-// for (var i = 0; i < question.length; i++) {
+var questionCount = 0;
+var score = 0;
+var question = document.getElementById("question");
+var answer = document.getElementById("answer");
 
 
-var response = document.getElementById("question");
-response.innerHTML = response.innerHTML.replace("", "Where are biggest waves in Oahu?"); /*I removed question[i]*/
 
-function checkAnswer(){
-  document.getElementById("answer").submit();
-    if (answer = "Waimea") {
+var questionList = [
+  "Where are biggest waves in Oahu?", 
+  "What is the freaky wave called near Portlock?", 
+  "What is the name of the beach park where Pipeline is located?", 
+  "What is a surfboard made of?", 
+  "What is the middle wodden spine of a surfboard called?", 
+  "What are young surfers called?"
+  ];
+
+var answerList = [
+  "Waimea", 
+  "China Walls", 
+  "Ehukai", 
+  "Foam", 
+  "Stringer", 
+  "Wax", 
+  "Groms"
+  ];
+
+
+document.getElementById("question").innerHTML = questionList[i]; 
+
+for (var i = 0; i < questionList.length; i++) 
+{
+  for( var j =0; j < answerList.length; j++) {
+  
+}
+  submit.onclick = function (){
+    document.getElementById("answer");
+      if (answer = answerList[i]) {
       alert("Right on!");
       ++score;
-      }
-    else {
+      } 
+      else {
       alert("Sorry, wrong answer!");
-    }
-};
+      }
+   }
+  }
+++ questionCount; 
+
+var finalScore = score.answer.length;
+alert(finalScore);
 
 
 })();
---
+++ dom_quiz2.js
@@ -0,0 +1,35 @@
+(function() {
+
+var question = document.getElementById("question");
+var answer = document.getElementById("answer")
+var questionCount = 0
+
+var quizQuestion = [
+  "Where are biggest waves in Oahu?", 
+  "What is the freaky wave called near Portlock?", 
+  "What is the name of the beach park where Pipeline is located?", 
+  "What is a surfboard made of?", 
+  "What is the middle wodden spine of a surfboard called?", 
+  "What do you put on surfboards to prevent slipping",
+  "What are young surfers called?"
+];
+
+
+var quizAnswer = [
+  "Waimea", 
+  "China Walls", 
+  "Ehukai", 
+  "Foam", 
+  "Stringer", 
+  "Wax",
+  "Groms"
+];
+
+question.innerHTML = quizQuestion[i];
+question.innerHTML = "Let's take a Surfing Quiz! Please hit Submit!";
+
+var hitSubmit = document.getElementById("submit");
+
+
+
+})();
